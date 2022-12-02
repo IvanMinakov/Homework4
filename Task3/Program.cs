22e8dff4b91476dc1b7f1a7d4adacb67d7494a73
Ivan Minakov <i.minakov786@gmail.com>
2022-12-02 06:21:05 -0500

    in progress

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = { 2, 4, 1, 8, 7, 5, 6, 3, 9 };
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+PrintArray(arr); 
