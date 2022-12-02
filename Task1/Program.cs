fc96bc12eeeb3e9d6ef50ab1b8d3ad542d9632f9
Ivan Minakov <i.minakov786@gmail.com>
2022-12-02 06:20:46 -0500

    task1 in progress

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+void Power(int c)
+{
+double power = (Math.Pow(a,b));
+}
+
+Console.WriteLine($"Число {a} в степени {b} = {power}");
